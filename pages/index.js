d5ef2c544623bb614413d0e968997c6efe9d9b9f
Karishma Dasgaonkar <karishmadasgaonkar@gmail.com>
2021-05-18 21:22:04 +0530

    pre-rendering and data fetching

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
+import { getSortedPostsData } from '../lib/posts'
 
-export default function Home() {
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -15,6 +16,45 @@ export default function Home() {
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p> */}
       </section>
+
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
     </Layout>
   )
 }
+
+//To use server-side rendering, use 'getServerSideProps(context)' instead of 'getStaticProps()'
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {allPostsData}
+  }
+
+  //Instead of the filesystem, you can fetch post data from an external API endpoint or Query Database
+  // const res = await fetch('..')
+  // return res.json()
+  //OR
+  //return somedbclient.query('select posts...')
+}
+//Client side rendering use SWR - a react hook for data fetching
+// import useSWR from 'swr'
+
+// function Profile() {
+//   const { data, error } = useSWR('/api/user', fetch)
+
+//   if (error) return <div>failed to load</div>
+//   if (!data) return <div>loading...</div>
+//   return <div>hello {data.name}!</div>
+// }
