c698f893141fc9650fc728acbfb6ef1a40bca3ae
Karishma Dasgaonkar <karishmadasgaonkar@gmail.com>
2021-05-19 00:16:58 +0530

    updated description

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hi! I'm a Full Stack developer with 3+ years of experience</p>
+        <p>Hi! I'm a full stack developer with 3+ years of experience</p>
         {/* <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
